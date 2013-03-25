fb41b1ae9854785283d4f907a571c60e4582cf9b
Conrad Calmez <conrad@calmez.net>
2013-03-25 20:43:04 +0100

    created empty view controller for recording view

---
+++ Recorder/CCRecordingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCRecordingViewController.h
+//  Recorder
+//
+//  Created by Conrad Calmez on 3/24/13.
+//  Copyright (c) 2013 Conrad Calmez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCRecordingViewController : UIViewController
+
+@end
---
+++ Recorder/CCRecordingViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCRecordingViewController.m
+//  Recorder
+//
+//  Created by Conrad Calmez on 3/24/13.
+//  Copyright (c) 2013 Conrad Calmez. All rights reserved.
+//
+
+#import "CCRecordingViewController.h"
+
+@interface CCRecordingViewController ()
+
+@end
+
+@implementation CCRecordingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
